6157432907ea385de37ffbcd1f09c0f157e5d8a8
al <alfredo.yap@stu.bmcc.cuny.edu>
2021-06-26 16:15:12 -0400

    Finished Lab 2 of Week 3

--- IBM_projs_coursse/Week3/lab2_template/adminsite/admin.py
+++ IBM_projs_coursse/Week3/lab2_template/adminsite/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
 from .models import Course, Instructor, Lesson
 
+class LessonInline(admin.StackedInline):
+    model = Lesson
+    extra = 5
+
+class CourseAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'name', 'description']
+    inlines = [LessonInline]
+
+class InstructorAdmin(admin.ModelAdmin):
+    fields = ['user', 'full_time', ]
+
+
 # Register your models here.
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Instructor, InstructorAdmin)
