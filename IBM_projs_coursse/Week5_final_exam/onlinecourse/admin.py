131fca3dbf5e7bd6b1229a8cf2377e1111690dd7
al <alfredo.yap@stu.bmcc.cuny.edu>
2021-06-30 20:55:27 -0400

    Finished Lab2 of Final Exam

---
+++ IBM_projs_coursse/Week5_final_exam/onlinecourse/admin.py
@@ -0,0 +1,40 @@
+from django.contrib import admin
+# <HINT> Import any new Models here
+from .models import Course, Lesson, Instructor, Learner, Question, Choice
+
+# <HINT> Register QuestionInline and ChoiceInline classes here
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 5
+
+class LessonInline(admin.StackedInline):
+    model = Lesson
+    extra = 5
+
+
+
+# Register your models here.
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [LessonInline]
+    list_display = ('name', 'pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['name', 'description']
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    pass
+
+
+# <HINT> Register Question and Choice models here
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Instructor)
+admin.site.register(Learner)
